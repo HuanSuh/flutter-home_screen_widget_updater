6f10e7c53e091d842c8b1ea9bf04dd12500a0e6a
Huan Suh <huansuh@gmail.com>
2020-05-31 14:25:51 +0900

    HomeScreenWidget (ios)

--- ios/Classes/SwiftHomeScreenWidgetUpdaterPlugin.swift
+++ ios/Classes/SwiftHomeScreenWidgetUpdaterPlugin.swift
@@ -3,31 +3,29 @@ import UIKit
 
 public class SwiftHomeScreenWidgetUpdaterPlugin: NSObject, FlutterPlugin {
     
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "home_screen_widget_updater", binaryMessenger: registrar.messenger())
-    let instance = SwiftHomeScreenWidgetUpdaterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "updateHomeScreenWidget":
-        if let jsonStr: String = call.arguments as? String, let json: [String:Any?] = convertToDictionary(text: jsonStr), let dataJsonStr: String = json["data"] as? String, let dataDict: [String:Any?] = convertToDictionary(text: dataJsonStr) {
-            print(dataJsonStr)
-            print(dataDict)
-            if let appGroupName = json["appGroupName"] as? String, let sharedContainer = UserDefaults(suiteName:  appGroupName) {
-                sharedContainer.set(dataDict as NSDictionary, forKey: "HOME_SCREEN_WIDGET_DATA_KEY")
-                result(true)
-                break
-            }
-        }
-        result(false)
-        break
-    default:
-        result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "home_screen_widget_updater", binaryMessenger: registrar.messenger())
+        let instance = SwiftHomeScreenWidgetUpdaterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
-    
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "updateHomeScreenWidget":
+            if let jsonStr: String = call.arguments as? String, let json: [String:Any?] = convertToDictionary(text: jsonStr), let dataJsonStr: String = json["data"] as? String, let dataDict: [String:Any?] = convertToDictionary(text: dataJsonStr) {
+                if let appGroupName = json["appGroupName"] as? String, let sharedContainer = UserDefaults(suiteName:  appGroupName) {
+                    sharedContainer.set(dataDict as NSDictionary, forKey: "HOME_SCREEN_WIDGET_DATA_KEY")
+                    result(true)
+                    break
+                }
+            }
+            result(false)
+            break
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
     func convertToDictionary(text: String) -> [String: Any]? {
         if let data = text.data(using: .utf8) {
             do {
